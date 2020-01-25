aa755652bdcfb4ab585d897e31dd947ed3a32ec4
Jason Aviles <sandman2k18@outlook.com>
2020-01-24 23:23:33 -0500

    update

---
+++ js/draw.js
@@ -0,0 +1,160 @@
+function draw8_none() {
+  var x = document.getElementById("draw8");
+
+    x.style.display = "none";
+    x.style.height = "2px"
+  
+}
+
+
+function draw8_display() {
+  var x = document.getElementById("draw8");
+
+    x.style.display = "block";
+    x.style.height = "auto"
+  
+}
+
+
+function draw7_none() {
+  var x = document.getElementById("draw7");
+
+    x.style.display = "none";
+    x.style.height = "2px"
+  
+}
+
+
+function draw7_display() {
+  var x = document.getElementById("draw7");
+
+    x.style.display = "block";
+    x.style.height = "auto"
+  
+}
+
+
+function draw6_none() {
+  var x = document.getElementById("draw6");
+
+    x.style.display = "none";
+    x.style.height = "2px"
+  
+}
+
+
+function draw6_display() {
+  var x = document.getElementById("draw6");
+
+    x.style.display = "block";
+    x.style.height = "auto"
+  
+}
+
+
+function draw5_none() {
+  var x = document.getElementById("draw5");
+
+    x.style.display = "none";
+    x.style.height = "2px"
+  
+}
+
+
+function draw5_display() {
+  var x = document.getElementById("draw5");
+
+    x.style.display = "block";
+    x.style.height = "auto"
+  
+}
+
+function draw4_none() {
+  var x = document.getElementById("draw4");
+
+    x.style.display = "none";
+    x.style.height = "2px"
+  
+}
+
+
+function draw4_display() {
+  var x = document.getElementById("draw4");
+
+    x.style.display = "block";
+    x.style.height = "auto"
+  
+}
+
+
+function draw3_none() {
+  var x = document.getElementById("draw3");
+
+    x.style.display = "none";
+    x.style.height = "2px"
+  
+}
+
+
+function draw3_display() {
+  var x = document.getElementById("draw3");
+
+    x.style.display = "block";
+    x.style.height = "auto"
+  
+}
+
+
+function draw2_none() {
+  var x = document.getElementById("draw2");
+
+    x.style.display = "none";
+    x.style.height = "2px"
+  
+}
+
+
+function draw2_display() {
+  var x = document.getElementById("draw2");
+
+    x.style.display = "block";
+    x.style.height = "auto"
+  
+}
+
+
+function draw1_none() {
+  var x = document.getElementById("draw1");
+
+    x.style.display = "none";
+    x.style.height = "2px"
+  
+}
+
+
+function draw1_display() {
+  var x = document.getElementById("draw1");
+
+    x.style.display = "block";
+    x.style.height = "auto"
+  
+}
+
+
+
+function draw_none() {
+  var x = document.getElementById("draw");
+
+    x.style.display = "none";
+    x.style.height = "2px"
+  
+}
+
+
+function draw_display() {
+  var x = document.getElementById("draw");
+
+    x.style.display = "block";
+    x.style.height = "auto"
+  
+}
