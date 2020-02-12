3d6cda648f0ff92afe49bb3822a482500f876aa6
Jason Aviles <sandman2k18@outlook.com>
2020-02-12 10:43:43 -0500

    update

--- js/draw.js
+++ js/draw.js
@@ -1,53 +1,62 @@
 function draw8_none() {
   var x = document.getElementById("draw8");
-
-    x.style.display = "none";
-    x.style.height = "2px"
+  
+  x.style.display = "block";
+ x.style.height = "0px"
+x.style.opacity='0'
   
 }
 
 
 function draw8_display() {
   var x = document.getElementById("draw8");
-
-    x.style.display = "block";
-    x.style.height = "auto"
+  x.style.transition= "all .7s linear"
+  x.style.display = "block";
+ x.style.height = "auto"
+ 
+ x.style.opacity='1'
   
 }
 
 
 function draw7_none() {
   var x = document.getElementById("draw7");
-
-    x.style.display = "none";
-    x.style.height = "2px"
-  
+ 
+     x.style.display = "block";
+    x.style.height = "0px"
+  x.style.opacity='0'
+ 
 }
 
 
 function draw7_display() {
   var x = document.getElementById("draw7");
-
-    x.style.display = "block";
+x.style.transition= "all .7s linear"
+     x.style.display = "block";
     x.style.height = "auto"
-  
+    
+    x.style.opacity='1'
 }
 
 
 function draw6_none() {
   var x = document.getElementById("draw6");
 
-    x.style.display = "none";
-    x.style.height = "2px"
   
+      x.style.display = "block";
+     x.style.height = "0px"
+   x.style.opacity='0'
 }
 
 
 function draw6_display() {
   var x = document.getElementById("draw6");
 
-    x.style.display = "block";
-    x.style.height = "auto"
+  x.style.transition= "all .7s linear"
+  x.style.display = "block";
+ x.style.height = "auto"
+ 
+ x.style.opacity='1'
   
 }
 
@@ -55,8 +64,10 @@ function draw6_display() {
 function draw5_none() {
   var x = document.getElementById("draw5");
 
-    x.style.display = "none";
-    x.style.height = "2px"
+  
+  x.style.display = "block";
+ x.style.height = "0px"
+x.style.opacity='0'
   
 }
 
@@ -64,16 +75,21 @@ function draw5_none() {
 function draw5_display() {
   var x = document.getElementById("draw5");
 
-    x.style.display = "block";
-    x.style.height = "auto"
+  x.style.transition= "all .7s linear"
+  x.style.display = "block";
+ x.style.height = "auto"
+ 
+ x.style.opacity='1'
   
 }
 
 function draw4_none() {
   var x = document.getElementById("draw4");
 
-    x.style.display = "none";
-    x.style.height = "2px"
+  
+  x.style.display = "block";
+ x.style.height = "0px"
+x.style.opacity='0'
   
 }
 
@@ -81,8 +97,11 @@ function draw4_none() {
 function draw4_display() {
   var x = document.getElementById("draw4");
 
-    x.style.display = "block";
-    x.style.height = "auto"
+  x.style.transition= "all .7s linear"
+  x.style.display = "block";
+ x.style.height = "auto"
+ 
+ x.style.opacity='1'
   
 }
 
@@ -90,8 +109,10 @@ function draw4_display() {
 function draw3_none() {
   var x = document.getElementById("draw3");
 
-    x.style.display = "none";
-    x.style.height = "2px"
+  
+      x.style.display = "block";
+     x.style.height = "0px"
+   x.style.opacity='0'
   
 }
 
@@ -99,8 +120,11 @@ function draw3_none() {
 function draw3_display() {
   var x = document.getElementById("draw3");
 
-    x.style.display = "block";
-    x.style.height = "auto"
+  x.style.transition= "all .7s linear"
+  x.style.display = "block";
+ x.style.height = "auto"
+ 
+ x.style.opacity='1'
   
 }
 
@@ -108,35 +132,43 @@ function draw3_display() {
 function draw2_none() {
   var x = document.getElementById("draw2");
 
-    x.style.display = "none";
-    x.style.height = "2px"
+  
+  x.style.display = "none";
+ x.style.height = "0px"
+x.style.opacity='0'
   
 }
 
 
 function draw2_display() {
   var x = document.getElementById("draw2");
-
-    x.style.display = "block";
-    x.style.height = "auto"
+  x.style.transition= "all .7s linear"
+  x.style.display = "block";
+ x.style.height = "auto"
+ 
+ x.style.opacity='1'
   
 }
 
 
 function draw1_none() {
   var x = document.getElementById("draw1");
-
-    x.style.display = "none";
-    x.style.height = "2px"
+x.style.background='#F7F7F7'
   
+  x.style.display = "block";
+ x.style.height = "0px"
+x.style.opacity='0'
 }
 
 
 function draw1_display() {
   var x = document.getElementById("draw1");
-
-    x.style.display = "block";
-    x.style.height = "auto"
+  x.style.background='#F7F7F7'
+  x.style.transition= "all .7s linear"
+  x.style.display = "block";
+ x.style.height = "auto"
+ 
+ x.style.opacity='1'
   
 }
 
@@ -145,8 +177,9 @@ function draw1_display() {
 function draw_none() {
   var x = document.getElementById("draw");
 
-    x.style.display = "none";
-    x.style.height = "2px"
+  x.style.display = "block";
+ x.style.height = "0px"
+x.style.opacity='0'
   
 }
 
@@ -154,7 +187,10 @@ function draw_none() {
 function draw_display() {
   var x = document.getElementById("draw");
 
-    x.style.display = "block";
+  x.style.transition= "all .7s linear"
+     x.style.display = "block";
     x.style.height = "auto"
+    
+    x.style.opacity='1'
   
 }
